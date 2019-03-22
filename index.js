3099bea679a438b0405573995fc3f70ef5b3164b
AlexandraMadalina <alexandramadalinacosma@gmail.com>
2019-03-22 20:05:49 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+window.onload = function() {
+  document.getElementById("doge").innerHTML = "Woof!";
+
+}
+var b
+function hover() {
+  document.getElementById("doge").style.display = "none";
+  document.getElementById("iconDoge").style.display = "inline";
+}
+
+document.getElementById("doge")
+
+  function showPicture() {
+var dogImg = document.getElementById("dogImg");
+   if (dogImg.style.display == 'none') {
+      dogImg.style.display = 'block';
+    } else {
+      dogImg.style.display = 'none';
+    }
+  }
+
+  /*function that creates random imgt objects and change the left and bottom atribute*/
+  /*a array of images*/
