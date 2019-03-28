8b3be4b5ae5fc1867ba94e0093fcf243574909ed
AlexandraMadalina <alexandramadalinacosma@gmail.com>
2019-03-28 16:46:31 +0100

    made random numbers of cat at click

---
+++ cat.js
@@ -0,0 +1,30 @@
+var button = document.getElementById("clickForMiaw");
+button.addEventListener("click", createCat);
+
+function createCat(){
+  
+  var catImg = ["img/cat1.png", "img/cat2.png", "img/cat3.png", "img/cat4.png", "img/cat5.png"];
+  var cat = document.createElement("img");
+  cat.src = catImg[Math.round(Math.random() * 4)];
+  cat.classList.add("nyan-cat-end");
+  document.body.appendChild(cat);
+  
+}
+
+
+   
+function updateTransition() {
+  
+  var cat = document.querySelector("img.nyan-cat-start");
+   
+  if (cat) {
+    cat.className = "nyan-cat-end";
+  } else {
+    cat = document.querySelector("img.nyan-cat-end");
+    cat.className = "nyan-cat-start";
+  }
+  
+  return cat;
+}
+
+var interval= window.setInterval(updateTransition, 4000);
--- index.js
+++ index.js
@@ -41,42 +41,52 @@ dogIcon.addEventListener("click", function() {
 
 dogImg.addEventListener("click", createCat);
 
+
+var catImg = ["img/cat1.png", "img/cat2.png", "img/cat3.png", "img/cat4.png", "img/cat5.png"];
+
+
 function createCat() {
-  var catImg = ["img/cat1.png", "img/cat2.png", "img/cat3.png", "img/cat4.png", "img/cat5.png"];
-  var cat = new Image(100, 100);
-  cat.src = catImg[Math.floor(Math.random() * 5)];
-  cat.style.position = "fixed";
-  x = (Math.floor(Math.random() * 8) * 100) - 500;
-  y = (Math.floor(Math.random() * 3) + 1) * -100;
-  cat.style.left = x + "px";
-  cat.style.top = y + "px";
-  document.body.appendChild(cat);
-  cat.classList.add("cats");
-}
-
-requestAnimationFrame(moveAllCats);
-
-
-
-function moveAllCats(){
-  var cats = document.getElementsByClassName("cats");
-  for(let i=0; i<cats.length; ++i){
-    moveCat(cats[i]);
+  var x = Math.round(Math.random() * 70);
+  for (i = 0; i <= x; ++i){
+    var cat = document.createElement("img");
+    cat.src = catImg[Math.round(Math.random() * 4)];
+    cat.style.position = "absolute";
+    setStartPosition(cat);
+    randomSpeed(cat);
+    cat.style.transitionTimingFunction = "ease-in";
+    cat.classList.add("nyan-cat");
+    document.body.appendChild(cat);
+    setEndPosition(cat);
   }
-
-  requestAnimationFrame(moveAllCats);
+  
 }
 
-function moveCat(cat) {
 
-var oldX = parseInt(cat.style.left, 10);
-var oldY = parseInt(cat.style.top, 10);
-oldX +=3;
-oldY +=3;
-cat.style.left = oldX + "px";
-cat.style.top = oldY + "px";
-if(oldY > 1100){
-cat.body.removeChild(cat);
+
+function setStartPosition(obj){
+  
+  var x = (Math.floor(Math.random() * 7) * 10) - 100;
+  var y = (Math.floor(Math.random() * 3) + 1) * -100;
+  obj.style.left = x + "vh";
+  obj.style.top = y + "vh";
+}
+
+function randomSpeed(obj){
+var s = Math.floor(Math.random()* 9);
+obj.style.transition = "all " + s + "s";
 
 }
+
+
+function setEndPosition(obj){
+  setTimeout(function(){ 
+  var x = (Math.floor(Math.random()* 7)*10) +50;
+  var y = (Math.floor(Math.random() * 1)+1) * 100;
+  var d = Math.floor(Math.random() * 1) * 360;
+  obj.style.left = x + "vw";
+  obj.style.top = y + "vh";
+  obj.style.transform = "rotate(" + d + "deg)";
+  console.log(obj.style);
+  }, 10);
 }
+
